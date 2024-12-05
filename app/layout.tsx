1aa937a01e415dbba8bb97a16fc519f2d596e7cc
Daekyo48 <karasin1013@gmail.com>
2024-12-05 14:34:47 +0900

    en -> ko

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="ko" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body className="font-sans">{children}</body>
       <SpeedInsights />
     </html>
