0b2aabacbbedb7ac970c9a53c6e54463ddc75f94
Daekyo48 <karasin1013@gmail.com>
2024-12-05 14:24:43 +0900

    error change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,4 @@
 import type { Metadata } from "next";
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
 import localFont from "next/font/local";
 import "./globals.css";
 
