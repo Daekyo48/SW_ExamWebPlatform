e6fcd34eee1690f4e18bf803f445bac307e1e6dc
Daekyo48 <karasin1013@gmail.com>
2024-12-05 18:52:48 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,8 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "지필평가 정보 플랫폼",
-  description: "-",
+  title: "지필평가 일정 플랫폼",
+  description: "지필평가 일정 안내 및 점수 계산을 지원합니다.",
 };
 
 export default function RootLayout({
